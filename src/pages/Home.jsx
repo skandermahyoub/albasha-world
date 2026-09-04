7bce0a85638a5315b339ddaa44560b992966c9f9
Oryx Business <skandermahyoub@gmail.com>
2026-09-04 17:44:27 +0000

    External agent changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ export default function Home() {
         base44.entities.HeroSlide.list('sort_order').catch(() => []),
         base44.entities.MarqueeText.list('sort_order').catch(() => []),
         base44.entities.SpecialOffer.list('sort_order').catch(() => []),
-        base44.entities.Product.list('-created_date', 50).catch(() => []),
+        base44.entities.Product.list('-created_date', 500).catch(() => []),
         base44.entities.Review.list('-created_date', 50).catch(() => []),
         base44.entities.Brand.list('sort_order').catch(() => []),
         base44.entities.BlogPost.list('-created_date', 10).catch(() => []),
@@ -76,7 +76,7 @@ export default function Home() {
       setSlides(slidesData);
       setMarqueeItems(marqueeData);
       setOffers(offersData);
-      setProducts(productsData);
+      setProducts(productsData.filter(product => product.status === 'active'));
       setReviews(reviewsData);
       setBrands(brandsData);
       setPosts(postsData);
