bbfc36842bf4cc38cc6ff234c1113b2690d2501b
Oryx Business <skandermahyoub@gmail.com>
2026-09-04 20:09:19 +0000

    External agent changes

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -35,7 +35,7 @@ export default function Shop() {
     const load = async () => {
       const [s, p, c] = await Promise.all([
         base44.entities.StoreSettings.list().catch(() => []),
-        base44.entities.Product.list('-created_date', 500).catch(() => []),
+        base44.entities.Product.list('-created_date', 1000).catch(() => []),
         base44.entities.Category.list('sort_order').catch(() => []),
       ]);
       setSettings(s[0] || {});
